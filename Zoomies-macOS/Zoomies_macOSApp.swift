55e414917036c71354b7454733ad0cbdf0f95448
Daniel <prillburn@gmail.com>
2026-04-10 23:30:02 +0100

    Initial Commit

---
+++ Zoomies-macOS/Zoomies_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zoomies_macOSApp.swift
+//  Zoomies-macOS
+//
+//  Created by Daniel Turrell on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Zoomies_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
